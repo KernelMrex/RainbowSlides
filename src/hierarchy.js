9702dafdca3981d39d6bfa6531a8630f13f3c998
Андрей Дудин <daa.and@mail.ru>
2020-09-25 19:35:22 +0300

    done hierarchy for review

---
+++ src/hierarchy.js
@@ -0,0 +1,58 @@
+var app = {
+    presentation: presentation,
+    history: [], //stack
+}
+
+var presentation = {
+    name: '',
+    slides: [], //linked list
+}
+
+var slide = {
+    objects: [],  
+    background: '', 
+}
+
+var text = {
+    name: 'Text 1',
+    position: {
+        x: 0,
+        y: 0
+    },
+    size: {
+        width: 1000,    
+    },
+    background: '#ffffff',
+    underline: '1px solid #ffffff'
+}
+
+var square = {
+    name: 'Square 1',
+    position: {
+        x: 0,
+        y: 0
+    },
+    size: {
+        height: 400,
+        width: 1000,    
+    },
+    background: '#ffffff',
+    lockedAspectRatio: true,
+}
+
+var circle = {
+    name: 'Circle 1',
+    position: {
+        x: 0,
+        y: 0
+    },
+    size: {
+        radius: 20,
+    },
+    background: '#ffffff',
+}
+
+var action = {
+    current: null,
+    previous: null,
+}
