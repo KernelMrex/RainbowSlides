596f75087a14a21c1a4227f74de310e08e041a6f
KernelMrex <24671528+KernelMrex@users.noreply.github.com>
2020-10-03 13:45:32 +0300

    RS-10 Fixed typos

--- src/modules/slides-object.ts
+++ src/modules/slides-object.ts
@@ -1,12 +1,12 @@
 function addObjectToSlide(app: App, slide: Slide, object: SlideObject): App
 {
     const slideIndex = app.presentation.slides.indexOf(slide);
-    const newSlides = [ ...app.presentation.slides ];
+    const newSlides = [...app.presentation.slides];
 
     newSlides[slideIndex] = {
         ...slide,
-        objects: [ ...slide.objects, object ]
-    }
+        objects: [...slide.objects, object]
+    };
 
     return Object.freeze({
         ...app,
@@ -20,12 +20,12 @@ function addObjectToSlide(app: App, slide: Slide, object: SlideObject): App
 function deleteObjectFromSlide(app: App, slide: Slide, object: SlideObject): App
 {
     const slideIndex = app.presentation.slides.indexOf(slide);
-    const newSlides = [ ...app.presentation.slides ];
+    const newSlides = [...app.presentation.slides];
 
     newSlides[slideIndex] = {
         ...slide,
-        objects: [ ...slide.objects ].splice(slide.objects.indexOf(object), 1)
-    }
+        objects: [...slide.objects].splice(slide.objects.indexOf(object), 1)
+    };
 
     return Object.freeze({
         ...app,
