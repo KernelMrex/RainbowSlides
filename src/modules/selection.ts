46c7ffe3594822b5dd0d51216655e917a9051aec
Андрей Дудин <daa.and@mail.ru>
2020-10-03 20:33:35 +0300

    after merge RS-8 change some problems with selection.ts

--- src/modules/selection.ts
+++ src/modules/selection.ts
@@ -3,7 +3,7 @@ function selectSlide(presentation: Presentation, slide: Slide): Presentation
     return {
         ...presentation,
         selection: {
-            slide: slide,
+            slide: slide.id,
             object: null
         }
     }
@@ -22,22 +22,35 @@ function deleteSlideFromSelection(presentation: Presentation): Presentation
 
 function selectObject(presentation: Presentation, object: SlidesObject): Presentation
 {
+    let objects: Array<SlidesObject>;
+    if (presentation.selection.object !== null) {
+        objects = [...presentation.selection.object];
+    } else {
+        objects = [];
+    }
+
+    objects.push(object);
     return {
         ...presentation,
         selection: {
             ...presentation.selection,
-            object: object
+            object: objects
         }
     }
 }
 
 function deleteObjectFromSelection(presentation: Presentation, object: SlidesObject): Presentation
 {
-    return {
-        ...presentation,
-        selection: {
-            ...presentation.selection,
-            object: null
+    if (presentation.selection.object !== null) {
+        let objects: Array<SlidesObject> = presentation.selection.object?.splice(presentation.selection.object?.indexOf(object), 1);
+        return {
+            ...presentation,
+            selection: {
+                ...presentation.selection,
+                object: objects
+            }
         }
     }
+
+    return presentation
 }
