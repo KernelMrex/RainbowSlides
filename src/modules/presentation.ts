c5567a7b2384bb27920b2f9b92227cd18dc79f95
Андрей Дудин <daa.and@mail.ru>
2020-10-03 13:16:34 +0300

    done module presentation

---
+++ src/modules/presentation.ts
@@ -0,0 +1,43 @@
+function createPresentation(name: string): App
+{
+    return{
+        presentation: {
+            name: name,
+            slides: [],
+            selection: {
+                slide: null,
+                object: null
+            }
+        },
+        history: {
+            undoStack: [],
+            redoStack: []
+        }
+    }
+}
+
+function deletePresentation(app: App): App
+{
+    return {
+        ...app,
+        presentation: {
+            name: '',
+            slides: [],
+            selection: {
+                slide: null,
+                object: null
+            }
+        }
+    }
+}
+
+function getJSONOfPresentation(presentation: Presentation): string
+{
+    return JSON.stringify(presentation)
+}
+
+function getPresentationFromJSON(presentationInJSON: string): Presentation
+{
+    return JSON.parse(presentationInJSON);
+}
+
