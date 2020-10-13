b9affa132b225ec59f00ea7a4a921918a067ba53
KernelMrex <24671528+KernelMrex@users.noreply.github.com>
2020-10-13 20:01:23 +0300

    RS-10 1) Added tests for removeObjectFromSlide function

--- src/core/slides-object.test.ts
+++ src/core/slides-object.test.ts
@@ -1,4 +1,4 @@
-import { addObjectToSlide } from './slides-object';
+import { addObjectToSlide, removeObjectFromSlide } from './slides-object';
 
 const mockPresentation: Presentation = {
     name: 'Mock presentation',
@@ -36,6 +36,7 @@ test('Add object to slide', () => {
     const object = { ...mockRectangle }
     const newPresentationInstance = addObjectToSlide(presentation, 'mock-slide', object)
     expect(newPresentationInstance).not.toBe(presentation)
+    expect(presentation.slides[0].objects).not.toContain(object)
     expect(newPresentationInstance.slides[0].objects).toContain(object)
 })
 
@@ -49,4 +50,40 @@ test('Add object to not existing slide', () => {
     const object = { ...mockRectangle }
     const newPresentationInstance = addObjectToSlide(presentation, 'no-existing-slide', object)
     expect(newPresentationInstance).toBe(presentation)
+    expect(newPresentationInstance.slides[0].objects).not.toContain(object)
+})
+
+test('Remove object from slide', () => {
+    const object = { ...mockRectangle }
+    const presentation: Presentation = {
+        ...mockPresentation,
+        slides: [
+            {
+                ...mockSlide,
+                objects: [ object ]
+            }
+        ]
+    }
+
+    const newPresentationInstance = removeObjectFromSlide(presentation, 'mock-slide', 'mock-rectangle')
+    expect(presentation.slides[0].objects).toContain(object)
+    expect(newPresentationInstance.slides[0].objects).not.toContain(object)
+})
+
+test('Remove object from not existing slide', () => {
+    const object = { ...mockRectangle }
+    const presentation: Presentation = {
+        ...mockPresentation,
+        slides: [
+            {
+                ...mockSlide,
+                objects: [ object ]
+            }
+        ]
+    }
+
+    const newPresentationInstance = removeObjectFromSlide(presentation, 'not-existing-slide', 'mock-rectangle')
+    expect(newPresentationInstance).toBe(presentation)
+    expect(presentation.slides[0].objects).toContain(object)
+    expect(newPresentationInstance.slides[0].objects).toContain(object)
 })
