2c617f43f3fe4c7ba1e254f958944addb64566e6
andreanapolicy <daa.and@mail.ru>
2020-10-26 21:01:51 +0300

    RS-19 подправил стили для кнопок. Начата работа по внедрению функционала экпорта и импорта

--- src/TopBar/TopBarButtons/TopBarButtons.tsx
+++ src/TopBar/TopBarButtons/TopBarButtons.tsx
@@ -6,13 +6,13 @@ export default function TopBarButtons()
     return (
         <div className={style.buttons_wrapper}>
             <div className={style.button_export}>
-                <button>export</button>
+                <button className={style.content}>export</button>
             </div>
             <div className={style.button_import}>
-                <button>import</button>
+                <button className={style.content}>import</button>
             </div>
             <div className={style.button_preview}>
-                <button>preview</button>
+                <button className={style.content}>preview</button>
             </div>
         </div>
     )
---
+++ src/view-module/exportPresentation.ts
@@ -0,0 +1,6 @@
+import {Presentation} from '../core/types';
+
+export function exportPresentation(presentation: Presentation): string
+{
+    return '';
+}
