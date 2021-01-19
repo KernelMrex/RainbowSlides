9db072d1ef045025a49f18a607dfc174ed651dfc
andreanapolicy <daa.and@mail.ru>
2021-01-19 11:30:34 +0300

    RS-36. Удалил ненужный файл

--- src/common/bindKeys.ts
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export function bindKeys(deleteObject: () => void, copyElement: (event?: React.MouseEvent<HTMLElement>) => void, pasteElement: (event?: React.MouseEvent<HTMLElement>) => void)
-{
-}
